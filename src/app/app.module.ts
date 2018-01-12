455129ce64cd988c4da6d748224bd21ec3f7f36f
Yennifer Pernia <perniayennifer@gmail.com>
2018-01-12 16:30:33 -0300

    inicio de proyecto componentes y servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AllPokemonComponent } from './components/all-pokemon/all-pokemon.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+
+import { ServicesService } from './services.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllPokemonComponent,
+    PokemonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [ServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class ServicesService {
+
+  urlPokemon:string = 'http://pokeapi.co/api/v2/';
+
+  constructor() { }
+
+}
